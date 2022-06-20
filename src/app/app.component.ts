5b1a977d5595162ae83c1a34b0fd3f49d39dbfac
Thodler <neoskywalker01@gmail.com>
2022-06-20 15:38:35 +0200

    [UPD] ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,32 @@ import {FaceSnap} from "./_models/FaceSnap";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  mySnapKenobi!: FaceSnap;
-  mySnapVador!: FaceSnap;
-  mySnapYoda!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(){
-    this.mySnapKenobi = {
-      title: "Kenobi",
-      description: "Le dernier Jedi",
-      imageUrl: "https://www.journaldugeek.com/content/uploads/2022/05/obi-wan-kenobi-critique-star-wars.jpg",
-      createdDate: new Date(),
-      snaps: 64308,
-      location: "Tatoine"
-    };
-    this.mySnapVador = {
-      title:"Dark Vador",
-      description: "Le Mechant",
-      imageUrl: "https://www.breizh-info.com/wp-content/uploads/2021/04/dark_vador.jpeg",
-      createdDate: new Date(),
-      snaps: 10062
-    };
-    this.mySnapYoda = {
-      title:"Yoda",
-      description: "Le grand maitre Jedi",
-      imageUrl: "https://www.programme-tv.net/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2EplurimediaNews.2F2018.2F01.2F11.2F99cdd0a3-823a-4ef7-902f-6f44aff8abfb.2Ejpeg/1200x600/crop-from/top/quality/80/star-wars-maitre-yoda-de-retour-dans-l-episode-8.jpg",
-      createdDate: new Date(),
-      snaps: 1000,
-      location: "Dagobah"
-    };
+    this.faceSnaps = [
+      {
+        title: "Kenobi",
+        description: "Le dernier Jedi",
+        imageUrl: "https://www.journaldugeek.com/content/uploads/2022/05/obi-wan-kenobi-critique-star-wars.jpg",
+        createdDate: new Date(),
+        snaps: 64308,
+        location: "Tatoine"
+      },{
+        title:"Dark Vador",
+        description: "Le Mechant",
+        imageUrl: "https://www.breizh-info.com/wp-content/uploads/2021/04/dark_vador.jpeg",
+        createdDate: new Date(),
+        snaps: 10062
+      },{
+        title:"Yoda",
+        description: "Le grand maitre Jedi",
+        imageUrl: "https://www.programme-tv.net/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2EplurimediaNews.2F2018.2F01.2F11.2F99cdd0a3-823a-4ef7-902f-6f44aff8abfb.2Ejpeg/1200x600/crop-from/top/quality/80/star-wars-maitre-yoda-de-retour-dans-l-episode-8.jpg",
+        createdDate: new Date(),
+        snaps: 1000,
+        location: "Dagobah"
+      }
+    ];
   }
 }
