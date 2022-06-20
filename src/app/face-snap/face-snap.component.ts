402c0994170f5d80fad886d3dce779ff0dbe4c2f
Thodler <neoskywalker01@gmail.com>
2022-06-20 15:30:09 +0200

    [UPD] Mise en forme

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FaceSnap} from "../_models/FaceSnap";
 
 @Component({
   selector: 'app-face-snap',
@@ -6,29 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit{
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageURL!: string;
+
+  @Input() faceSnap!: FaceSnap;
+
   ohSnapBtn!: string;
 
   ngOnInit(): void {
-    this.title = "Kenobi";
-    this.description = "Le dernier Jedi";
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageURL = "https://www.journaldugeek.com/content/uploads/2022/05/obi-wan-kenobi-critique-star-wars.jpg";
     this.ohSnapBtn = "Oh Snap !";
   }
 
   onClickSnap() {
     if(this.ohSnapBtn == "Oh Snap !"){
       this.ohSnapBtn = "Oops, Oh Snap !"
-      this.snaps++;
+      this.faceSnap.snaps++;
     } else {
       this.ohSnapBtn = "Oh Snap !"
-      this.snaps--;
+      this.faceSnap.snaps--;
     }
   }
 
