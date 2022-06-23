f85178c2e8d4befe1b357e0be215cd0250d042b3
Thodler <neoskywalker01@gmail.com>
2022-06-23 16:36:32 +0200

    [UPD] Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FaceSnap} from "./_models/FaceSnap";
+import {map, filter, interval, Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,17 @@ import {FaceSnap} from "./_models/FaceSnap";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  interval$!: Observable<string>;
   ngOnInit(){
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? `Je suis ${value} et je suis pair` : `Je suis ${value} et je suis impair`),
+      tap(value => this.logger(value))
+    );
+  }
 
+  logger(text: string): void
+  {
+    console.log(`Log: ${text}`);
   }
 }
