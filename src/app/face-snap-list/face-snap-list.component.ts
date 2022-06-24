75354c88cfcb2fa1238dce94f31d7d5d58b1c589
Thodler <neoskywalker01@gmail.com>
2022-06-24 11:30:20 +0200

    [UPD] FIN Observable

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FaceSnap} from "../_models/FaceSnap";
 import {FaceSnapsService} from "../_services/face-snaps.service";
+import {interval, Subject, take, takeUntil, tap} from "rxjs";
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,13 +11,22 @@ import {FaceSnapsService} from "../_services/face-snaps.service";
 export class FaceSnapListComponent implements OnInit {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(
     private faceSnapService: FaceSnapsService
   ) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapService.getAllFaceSnaps();
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
 }
